455951d64fbbe8c74d2816c9fc2d502beb6fa3df
Janhavi Singh <125474457+Janhavi-alt-glitch@users.noreply.github.com>
2023-05-23 14:08:40 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
  function Footer(){
     return(
         <div className="container"style={{display:"flex",  justifyContent:"end", fontSize:"9px", position:"fixed",bottom:"0",marginLeft:"100px"}}>
-      <hr/>
+     
      <p>@2023 Janhavi<br/>
             ALL RIGHT RESERVED
        </p>
